d347f8ddea5b2dc12b6e841d1de74d163d12f819
Sebastian Nepote <sebastian.nepote@helpling.com>
2015-11-20 18:11:08 +0100

    fix double and single quoutes

--- playground/array.rb
+++ playground/array.rb
@@ -1,36 +1,35 @@
 def squares_seq(min = 1, max = 1)
-    puts (min..max).map { |i| i**2 }.join(", ")
+    puts (min..max).map { |i| i**2 }.join(', ')
 end
 
 # example
-puts "squares_seq(1, 5)"
+puts 'squares_seq(1, 5)'
 squares_seq(1, 5)
 
 puts "%w[a, b, c] returns an array of letters #{%w[a, b, c]}"
 
 puts "%w[a, b, c].to_s.upcase returns #{%w[a, b, c].to_s.upcase}"
 puts "%w[a, b, c].map { |char| char.upcase}.join returns #{%w[a, b, c].map { |char| char.upcase}.join}"
-puts "%w[a, b, c].map(&:upcase).join(" ") returns #{%w[a, b, c].map(&:upcase).join(" ")}"
+puts "%w[a, b, c].map(&:upcase).join(\" \") returns #{%w[a, b, c].map(&:upcase).join(' ')}"
 
 # Hash
-puts "// Hash"
+puts '// Hash'
 
-user = { :first_name => "Sebastian", :last_name => "Nepote" }
+user = { :first_name => 'Sebastian', :last_name => 'Nepote' }
 puts 'user = { :first_name => "Sebastian", :last_name => "Nepote" } returns ' + user.to_s
-puts "/ Another notation (not a symbol, but it will be converted to a symbol)"
-user = { first_name: "Sebastian", last_name: "Nepote" }
-puts 'suser = { first_name: "Sebastian", last_name: "Nepote" } returns ' + user.to_s
+puts '/ Another notation (not a symbol, but it will be converted to a symbol)'
+user = { first_name: 'Sebastian', last_name: 'Nepote' }
+puts 'user = { first_name: "Sebastian", last_name: "Nepote" } returns ' + user.to_s
 
 params = {}
-params[:user] = { name: "Sebastian Nepote", email: "snepote@gmail.con" }
+params[:user] = { name: 'Sebastian Nepote', email: 'snepote@gmail.con' }
 puts 'params = {}'
 puts 'params[:user] = { name: "Sebastian Nepote", email: "snepote@gmail.con" } returns ' + params.to_s
 puts "params[:user][:name] returns #{params[:user][:name]}"
 
-flash = { success: "It worked!", danger: "It failed." }
+flash = { success: 'It worked!', danger: 'It failed.' }
 puts "flash = #{flash}"
-puts 'flash.each { |key, value| puts "#{key.inspect} => #{value}" }'
+puts "flash.each { |key, value| puts \"#{key.inspect} => #{value}\" }"
 flash.each { |key, value| puts "#{key.inspect} => #{value}" }
 puts "p flash.keys returns #{p flash.keys} which is an array with the symbols"
 puts "while push flash.keys returns #{puts flash.keys} which is the symbols represented as strings"
-
