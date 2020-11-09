f0ab184591bb1350f0bfb81752df99b39af7f3c1
Sebastian Nepote <sebastian.nepote@helpling.com>
2020-11-09 23:06:35 +0100

    landing page generator with Responsive Design CSS

--- page-generator/generator.rb
+++ page-generator/generator.rb
@@ -2,19 +2,15 @@ require 'csv'
 require 'slugify'
 
 PAGES_DIRECTORY = 'pages'
-BLOCK_ITEM_REPLACEMENT = "<tr>
-    <td>%{name}</td>
-    <td>%{email}</td>
-    <td>%{service}</td>
-</tr>"
+block = File.open('block.html').read
 
 # Read CVS and group content
-items = CSV.open('data.csv', col_sep: ';', headers: :first_row).map(&:to_h)
+items = CSV.open('data - vertical.CSV', col_sep: ',', headers: :first_row).map(&:to_h)
 pages = {}
 items.each do |item|
   name = "#{item['service']}_#{item['location']}".slugify
   pages[name] = {title: "#{item['service']} in #{item['location']}", filename: "#{name}.html", block: ''} unless pages.key?(name)
-  pages[name][:block] = pages[name][:block] + (BLOCK_ITEM_REPLACEMENT % item.transform_keys(&:to_sym)).to_s
+  pages[name][:block] = pages[name][:block] + (block % item.transform_keys(&:to_sym)).to_s
 end
 
 # Deletes all existing files in target directory
