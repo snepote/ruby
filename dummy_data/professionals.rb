94dca56aeab28a5fca096620fcaecad88336bb3e
Sebastian Nepote <sebastian.nepote@helpling.com>
2020-11-11 16:22:13 +0100

    Dummy data generator

---
+++ dummy_data/professionals.rb
@@ -0,0 +1,27 @@
+require 'faker'
+require 'json'
+require 'slugify'
+
+profession = []
+3.times do
+  profession << Faker::Company.profession
+end
+
+puts "The following profession will be included: #{profession.join(', ')}"
+puts "in the following cities"
+puts cities = ['Berlin', 'Hamburg']
+
+File.open('professionals.csv', 'w') do |f|
+  f.puts 'name,email,service,location,company,logo,avatar'
+
+  30.times do
+    name     = Faker::Name.name
+    email    = Faker::Internet.email
+    service  = profession.sample
+    location = cities.sample
+    company  = "\"#{Faker::Company.name} #{Faker::Company.suffix}\""
+    avatar   = Faker::Avatar.image(slug: name.to_s.slugify, size: '50x50', format: 'jpg')
+    logo     = Faker::Company.logo.to_s.sub('medium', 'small')
+    f.puts [name, email, service, location, company, logo, avatar].join(',')
+  end
+end
