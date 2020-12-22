dd6f24c1f11e672c456ae926c1eb4c055e1126d7
Sebastian Nepote <sebastian.nepote@helpling.com>
2020-12-22 13:26:19 +0100

    Dummy data generator update

--- dummy_data/professionals.rb
+++ dummy_data/professionals.rb
@@ -3,18 +3,18 @@ require 'json'
 require 'slugify'
 
 profession = []
-3.times do
+5.times do
   profession << Faker::Company.profession
 end
 
 puts "The following profession will be included: #{profession.join(', ')}"
 puts "in the following cities"
-puts cities = ['Berlin', 'Hamburg']
+puts cities = ['Berlin', 'Hamburg', 'Frankfurt']
 
 File.open('professionals.csv', 'w') do |f|
   f.puts 'name,email,service,location,company,logo,avatar'
 
-  30.times do
+  80.times do
     name     = Faker::Name.name
     email    = Faker::Internet.email
     service  = profession.sample
